51e2a1a7c4553b048b5c3ab9b2c2af00fd1fd4ed
ptvan <pvan@fredhutch.org>
2020-05-16 06:40:23 -0700

    updates

--- batchRenamer_tkinter.py
+++ batchRenamer_tkinter.py
@@ -1,4 +1,5 @@
 from tkinter import INSERT, Tk, Frame, Button, Entry, Checkbutton, Text, IntVar, filedialog, messagebox
+import sys
 
 class batchRenamer(Frame):
 
@@ -13,38 +14,41 @@ class batchRenamer(Frame):
         self.browseBtn.grid(row=0, column=0)
 
         self.oldNames = Text(self, width=50, height=5)
-        self.oldNames.grid(row=1, column=0, columnspan=4)
+        self.oldNames.grid(row=1, column=0, columnspan=3)
         
-        prefixOn = IntVar()
-        self.prefixChkbox = Checkbutton(self, text="prefix", variable=prefixOn) 
+        self.prefixOn = IntVar()
+        self.prefixChkbox = Checkbutton(self, text="prefix", variable=self.prefixOn) 
         self.prefixChkbox.grid(row=2, column=0)
-        self.prefixEdit = Entry(self)
+        self.prefixEdit = Entry(self, width=10)
         self.prefixEdit.grid(row=2, column=1)
 
-        suffixOn = IntVar()
-        self.suffixChkbox = Checkbutton(self, text="suffix", variable=suffixOn)
+        self.suffixOn = IntVar()
+        self.suffixChkbox = Checkbutton(self, text="suffix", variable=self.suffixOn)
         self.suffixChkbox.grid(row=3, column=0) 
-        self.suffixEdit = Entry(self)
+        self.suffixEdit = Entry(self, width=10)
         self.suffixEdit.grid(row=3, column=1)
 
-        numberingOn = IntVar()
-        self.numberingChkbox = Checkbutton(self, text="numbering", variable=numberingOn)
+        self.numberingOn = IntVar()
+        self.numberingChkbox = Checkbutton(self, text="numbering", variable=self.numberingOn)
         self.numberingChkbox.grid(row=4, column=0) 
-        self.numberingStartEdit = Entry(self)
+        self.numberingStartEdit = Entry(self, width=5)
         self.numberingStartEdit.grid(row=4, column=1)
-        self.numberingStepEdit = Entry(self)
+        self.numberingStepEdit = Entry(self, width=5)
         self.numberingStepEdit.grid(row=4, column=2)
 
-        replaceOn = IntVar()
-        self.replaceChkbox = Checkbutton(self, text="replace", variable=replaceOn)
+        self.replaceOn = IntVar()
+        self.replaceChkbox = Checkbutton(self, text="replace", variable=self.replaceOn)
         self.replaceChkbox.grid(row=5, column=0) 
         self.replaceOldEdit = Entry(self)
         self.replaceOldEdit.grid(row=5, column=1)
         self.replaceNewEdit = Entry(self)
         self.replaceNewEdit.grid(row=5, column=2)
+
+        self.newNames = Text(self, width=50, height=5)
+        self.newNames.grid(row=6, column=0, columnspan=3)
         
         self.renameBtn = Button(self, text="rename", command=self.confirm)
-        self.renameBtn.grid(row=6, column=0)
+        self.renameBtn.grid(row=7, column=0)
 
         
     def browseFiles(self):
@@ -52,8 +56,24 @@ class batchRenamer(Frame):
         print(files)
         self.oldNames.insert(INSERT, files)
 
+    def processNames(self):
+        if self.prefixOn == True:
+            print("prefix")
+
+        if self.suffixOn == True:
+            print("suffix")
+        
+        if self.numberingOn == True:
+            print("numbering")
+        
+        if self.replaceOn == True:
+            print("replace")
+
     def confirm(self):
         messagebox.askyesno('Confirm file rename', 'Rename files ?')
+    
+    def renameFiles(self):
+        print("renaming files!")
 
 
 root = Tk()
